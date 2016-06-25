2fc29fc6179b1c0ffcb21ef3f7568367c28e66a5
jrootham <jrootham@gmail.com>
2016-06-25 00:43:59 -0400

    webpack production

---
+++ webpack.production.config.js
@@ -0,0 +1,36 @@
+/**
+ * webpack.production.config.js.js
+ *
+ * Created by jrootham on 23/06/16.
+ *
+ * Copyright © 2016 Jim Rootham
+ */
+var webpack = require("webpack");
+var config = require("./webpack.config.js");
+
+config.module.loaders[0].query = {
+    stage: 0
+    , optional: [
+        "optimisation.react.constantElements"
+        , "optimisation.react.inlineElements"
+    ]
+}
+
+if(!config.plugins) config.plugins = [];
+
+config.plugins.push(
+    new webpack.DefinePlugin({
+        __DEV__: false
+        , "process.env.NODE_ENV": JSON.stringify("production")
+    })
+    , new webpack.optimize.UglifyJsPlugin()
+    , new webpack.optimize.DedupePlugin()
+//, new webpack.optimize.MinChunkSizePlugin({
+//    minChunkSize: 10000
+//  })
+    , new webpack.optimize.AggressiveMergingPlugin({ moveToParents: true })
+)
+
+delete config.devtool;
+
+module.exports = config;
